96319fc2fd3c8666c6474715d278682f154b6672
tranthuc2000 <thuctran@antking.asia>
2025-02-12 16:53:10 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Thục on 12/2/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Thục on 12/2/25.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
